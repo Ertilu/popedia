96ded52db2989fe0ffd092e3fc5860baaea3d282
vindi pop <ardino.school@gmail.com>
2019-09-24 09:28:55 +0700

    new module

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Fragment } from 'react';
 import {
   SafeAreaView,
@@ -24,64 +16,10 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
-
 const App = () => {
   return (
     <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <Container>
-              <Header searchBar rounded>
-                <Item>
-                  <Icon name="ios-search" />
-                  <Input placeholder="Search" />
-                  <Icon name="ios-people" />
-                </Item>
-                <Button transparent>
-                  <Text>Misterius</Text>
-                </Button>
-              </Header>
-            </Container>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>POPEDIA</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>Apaa.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <View><Text>Lets get oke</Text></View>
     </Fragment>
   );
 };
