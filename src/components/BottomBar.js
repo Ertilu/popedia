a3aa15e77c063a5d7e4246e544605c4051ee28b6
ERTILU <rezaraka00@gmail.com>
2019-09-27 01:01:47 +0700

    bottom bar

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -35,7 +35,7 @@ export default class BottomBar extends Component {
     //   image: 'https://ecs7.tokopedia.net/img/cache/100-square/attachment/2019/1/11/20723472/20723472_8e83f5b5-b78a-477f-a28f-c416e5249cd0.png.webp'
     // },
     const navs = [
-      {
+       {
         icon: "heart",
         text: 'Feed',
         image: 'https://ecs7.tokopedia.net/img/cache/100-square/attachment/2019/1/9/20723472/20723472_7b72c368-d93e-41e1-8889-ef695ac5dc97.png.webp'
@@ -47,26 +47,65 @@ export default class BottomBar extends Component {
       },
     ]
 
-    return (
-      <Footer>
-        <View style={styles.wrapper} >
+    // , {
+    //   icon: "md-person",
+    //   text: 'Akun',
+    //   image: 'https://ecs7.tokopedia.net/img/cache/100-square/attachment/2019/1/9/20723472/20723472_a42a010b-bd35-4279-8bea-84e7bb1bcfc0.png.webp'
+    // }
+
+    return(
+        <Footer>
+          <View style={styles.wrapper} >
+            <TouchableOpacity onPress={() => navigation.navigate("Home")}> 
+              <View
+                style={styles.button}
+                >
+                <Image
+                  source={{uri: 'https://ecs7.tokopedia.net/img/cache/100-square/attachment/2019/1/11/20723472/20723472_8e83f5b5-b78a-477f-a28f-c416e5249cd0.png.webp'}}
+                  style={styles.menuIcon}
+                />
+                  <Text numberOfLines={1} style={styles.text}>Home</Text>
+              </View>
+            </TouchableOpacity>
           {
             navs.map(item =>
-              <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-                <View
-                  style={styles.button}
-                >
-                  <Image
-                    source={{ uri: item.image }}
-                    style={styles.menuIcon}
-                  />
-                  <Text numberOfLines={1} style={styles.text}>{item.text}</Text>
-                </View>
-              </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate("Maintenance")}> 
+            <View
+              style={styles.button}
+              >
+              <Image
+                source={{uri: item.image}}
+                style={styles.menuIcon}
+              />
+                <Text numberOfLines={1} style={styles.text}>{item.text}</Text>
+            </View>
+            </TouchableOpacity>
             )
           }
-        </View>
-      </Footer>
+            <TouchableOpacity onPress={() => navigation.navigate("Cart")}> 
+              <View
+                style={styles.button}
+                >
+                <Image
+                  source={{uri: 'https://ecs7.tokopedia.net/img/cache/100-square/attachment/2019/1/14/20723472/20723472_42491eb8-4d07-44f0-9a79-03ea94fdfd27.png.webp'}}
+                  style={styles.menuIcon}
+                />
+                  <Text numberOfLines={1} style={styles.text}>Keranjang</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate("Account")}> 
+            <View
+              style={styles.button}
+              >
+              <Image
+                source={{uri: 'https://ecs7.tokopedia.net/img/cache/100-square/attachment/2019/1/9/20723472/20723472_a42a010b-bd35-4279-8bea-84e7bb1bcfc0.png.webp'}}
+                style={styles.menuIcon}
+              />
+                <Text numberOfLines={1} style={styles.text}>Akun</Text>
+            </View>
+            </TouchableOpacity>
+          </View>
+        </Footer>
     )
   }
 } 
